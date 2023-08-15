db56a1db8c12a0940b1320384b572a2d055966c4
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-15 00:43:05 -0300

    add Film Controller new route improve filmController to a static class

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import index from './controller/filmController.js';
+import FilmController from './controller/FilmController.js';
 import ShowController from './controller/ShowController.js'
 
 const Routes = express.Router();
 
-Routes.get('/film', index);
+Routes.get('/film/:idShow/:videoTitle', FilmController.index);
 Routes.get('/show/:id/videos', ShowController.index);
 Routes.get('/show/:id/videos/:title', ShowController.get);
 //Routes.get('', (req, res) => {})
--- src/controller/filmController.js
+++ src/controller/filmController.js
@@ -1,8 +1,27 @@
+import HttpHelper from '../helper/HttpHelper.js';
+import { getShow } from '../service/ShowService.js';
 import getVideoStream from '../service/videoStreamService.js';
+import VideoService from '../service/VideoService.js';
 
+export default class FilmController {
+    static videoSerivce = new VideoService();
 
-export default function index(req, res) {
-    getVideoStream(
-        "J:/Downloads/Como Eu Conheci Sua Mãe 2007 - 3ª Temporada Completa [WEB-DL] WWW.BLUDV.COM/How.I.Met.Your.Mother.S03E01.720p.WEB-DL.6CH.x264.DUAL-WWW.BLUDV.COM.mkv",
-        req.headers.range, res);
+    
+    static index(req, res) {
+        const showFind = getShow(req.params.idShow);
+        if(!showFind)
+            return res.status(HttpHelper.NOT_FOULD).send({error: "Não encontramos o show solicitado"});
+
+        const ListVideos = FilmController.videoSerivce.getVideosAndGiveTitle(showFind.pathFolder, "S3E{e}");
+        const findVideo = FilmController.videoSerivce.findOneByTitle(req.params.videoTitle, ListVideos);
+
+        if(!findVideo)
+            return res.status(HttpHelper.NOT_FOULD).send({error: "Não encontramos o video desse show"});
+
+        let videoPath = showFind.pathFolder + findVideo.fileName;
+
+        return getVideoStream(
+            videoPath,
+            req.headers.range, res);
+    }
 }
