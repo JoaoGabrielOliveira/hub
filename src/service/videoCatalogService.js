890e3e2430483426eff12855b5154f711f71e7ae
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-14 23:53:37 -0300

    improve Video Catalog service

--- src/service/videoCatalogService.js
+++ src/service/videoCatalogService.js
@@ -4,19 +4,14 @@ import Video from '../model/Video.js';
 
 var index_id_example = 0;
 
-function listAllVideos(path, stringFormat = "{e}"){
-    let internalIndex = 0;
+export function listAllVideos(path, stringFormat = "{e}"){
+    let internalIndex = 1;
     return getAllVideosOfAPath(path).map(file => {
         return new Video(index_id_example++, 3, file.name, stringFormat.replace("{e}", internalIndex), internalIndex++)
     });    
 }
 
-function getAllVideosOfAPath(path){
+export function getAllVideosOfAPath(path){
     return fs.readdirSync(path, {withFileTypes:true})
     .filter(file => isFileAVideo(file.name));
 }
-
-console.log(
-    listAllVideos("J:/Downloads/Como Eu Conheci Sua Mãe 2007 - 3ª Temporada Completa [WEB-DL] WWW.BLUDV.COM/", "S3E{e}")
-)
-
