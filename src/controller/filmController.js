29f517760157ccee3eff81664557b12828a85700
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-14 23:20:06 -0300

    improve filmController add videoStreamService

--- src/controller/filmController.js
+++ src/controller/filmController.js
@@ -1,48 +1,8 @@
-import fs from 'fs';
-var filePath = "J:/Downloads/Como Eu Conheci Sua Mãe 2007 - 3ª Temporada Completa [WEB-DL] WWW.BLUDV.COM/S3E1.mkv";
+import getVideoStream from '../service/videoStreamService.js';
+
 
 export default function index(req, res) {
-    if(!fs.existsSync(filePath)){
-        res.status(404).send({
-            message: "Arquivo não encontrado"
-        });
-        return res;
-    }
-
-    const fileSize = fs.statSync(filePath).size;
-    const range = req.headers.range;
-
-    if(range){
-        var response = getStartAndEndOfRange(range, fileSize);
-        console.log("chuckSize:", response.chuckSize);
-
-        const file = fs.createReadStream(filePath, response);
-        const head = {
-            'Content-Range': `bytes ${response.start}-${response.end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': response.chuckSize,
-            'Content-Type': 'video/mp4'
-        };
-
-        res.writeHead(206, head);
-        file.pipe(res);
-    }
-    else {
-        const head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4'
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(filePath).pipe(res)
-    }
-}
-
-function getStartAndEndOfRange(range, size) {
-    const parts = range.replace(/bytes=/, '').split('-');
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : size - 1;
-
-    return {
-        start: start, end: end, chuckSize: end - start +  1
-    }
+    getVideoStream(
+        "J:/Downloads/Como Eu Conheci Sua Mãe 2007 - 3ª Temporada Completa [WEB-DL] WWW.BLUDV.COM/How.I.Met.Your.Mother.S03E01.720p.WEB-DL.6CH.x264.DUAL-WWW.BLUDV.COM.mkv",
+        req.headers.range, res);
 }
