e0aba0f7b75600c63d47fe0b7dc9baee35dc09ce
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-15 00:07:06 -0300

    add show model

---
+++ src/model/Show.js
@@ -0,0 +1,11 @@
+export default class Show {
+    id;
+    title;
+    pathFolder;
+
+    constructor(id, title, pathFolder){
+        this.id = id;
+        this.title = title;
+        this.pathFolder = pathFolder;
+    }
+}
