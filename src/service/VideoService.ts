ec863113f848d8e4c6cb22965ede47915e8ca6dc
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-16 22:14:07 -0300

    add methods to list all videos on a folder

--- src/service/VideoService.ts
+++ src/service/VideoService.ts
@@ -1,5 +1,7 @@
 import { AppDataSource } from "../database/DataSource";
+import { Show } from "../model/Show";
 import Video from "../model/Video";
+import getAllVideosOfAPath from "../util/FileHelper";
 import { BaseService } from "./BaseService";
 
 export default class VideoService extends BaseService<Video> {
@@ -9,6 +11,24 @@ export default class VideoService extends BaseService<Video> {
         super();
         this.repository = AppDataSource.getRepository(Video);
         this.nameTable = "Videos";
+    }
+
+    findAllVideoInFolderShow(show : Show, titleFormat : string = "{e}"){
+        let internalIndex = 0;
+        return getAllVideosOfAPath(show.pathFolder)
+            .map(video => {
+                internalIndex++;
+                return new Video()
+                    .setIdShow(show.id)
+                    .setFileName(video.name)
+                    .setTitle(titleFormat.replace("{e}", internalIndex.toString()))
+                    .setNumberOrder(internalIndex);
+            });
+    }
+
+    createVideosByShow(show : Show, titleFormat : string = "{e}") {
+        const videos = this.findAllVideoInFolderShow(show, titleFormat);
+        return this.saveAll(videos);
     }
 
     static getInstance() : VideoService{
