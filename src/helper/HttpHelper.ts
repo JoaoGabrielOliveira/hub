ca95e868b20d3f61ecad37c3564889881affb7dd
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-16 21:52:33 -0300

    add http helper

---
+++ src/helper/HttpHelper.ts
@@ -0,0 +1,9 @@
+import http2 from 'http2';
+
+export default class HttpHelper {
+    static get NOT_FOUND () { return HttpHelper.CODE.HTTP_STATUS_NOT_FOUND }
+
+    static get CODE () {
+        return http2.constants
+    }
+}
