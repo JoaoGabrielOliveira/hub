85994f4c20fede18e0780be374e53f5bc0e462e7
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-16 21:52:17 -0300

    add cors middleware in server.ts

--- src/config/Server.ts
+++ src/config/Server.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 import Routes from './Router';
+import cors from 'cors';
 
 const ExpressServer = express();
+ExpressServer.use(cors({
+    origin: '*'
+}))
 ExpressServer.use(express.json())
 ExpressServer.use( Routes);
 
