1fc667431ba1e9c5a2a0c2349aaaaec06e6ef0c5
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-17 00:09:02 -0300

    add bad request to http helper

--- src/helper/HttpHelper.ts
+++ src/helper/HttpHelper.ts
@@ -2,6 +2,7 @@ import http2 from 'http2';
 
 export default class HttpHelper {
     static get NOT_FOUND () { return HttpHelper.CODE.HTTP_STATUS_NOT_FOUND }
+    static get BAD_REQUEST () { return HttpHelper.CODE.HTTP_STATUS_BAD_REQUEST }
 
     static get CODE () {
         return http2.constants
