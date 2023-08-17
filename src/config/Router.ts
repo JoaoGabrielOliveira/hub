dc48ff1293e4113f7e9e50f1795a1b1a13ba86c2
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-17 00:33:57 -0300

    add improvised front-end

--- src/config/Router.ts
+++ src/config/Router.ts
@@ -1,6 +1,8 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import FilmController from "../controller/FilmController";
 import ShowController from "../controller/ShowController";
+import fs from "fs";
+import path from "path";
 
 const Routes = Router({});
 
@@ -11,5 +13,11 @@ Routes.get("/show/:id", ShowController.get);
 Routes.get("/show/:id/videos", ShowController.getVideos);
 Routes.get("/show/:id/videos/folder", ShowController.getVideosInFolder);
 Routes.post("/show/:id/videos", ShowController.postVideos);
+Routes.post("/show/:id/videos", ShowController.postVideos);
+
+
+Routes.get("/", (req, res) => res.sendFile(path.resolve("src/public/index.html")));
+Routes.get("/filme/show", (req, res) => res.sendFile(path.resolve("src/public/show.html")));
+
 
 export default Routes;
