c48b3dcd47b8e3e57b74757a89e13912a96eed65
João Gabriel Oliveira <joaogabrieloliveira81@gmail.com>
2023-08-25 01:54:43 -0300

    improve base service logging

--- src/service/BaseService.ts
+++ src/service/BaseService.ts
@@ -14,64 +14,64 @@ export abstract class BaseService<T extends BaseEntity> implements Service<T> {
     }
 
     async find(options: FindManyOptions<T>) {
-        SendEvent(`Starting search for ${this.name} in database!`, options, 'info');
+        SendEvent(`${this.constructor.name} - ${this.constructor.name} - Starting search for ${this.name} in database!`, options, 'info');
 
         const models = this.repository.find(options);
         models.then(result => {
             if(result)
-                SendEvent(`Search for ${this.name} completed successfully!`, options);
+                SendEvent(`${this.constructor.name} - ${this.constructor.name} - Search for ${this.name} completed successfully!`, options);
             else
-                SendEvent(`Has no ${this.name} in database!`, options, 'warn');
+                SendEvent(`${this.constructor.name} - Has no ${this.name} in database!`, options, 'warn');
         }).catch((error) => {
-            SendEvent(`Search for ${this.name} has a error!`, error, 'error');
+            SendEvent(`${this.constructor.name} - Search for ${this.name} has a error!`, error, 'error');
         });
 
         return models;
     }
 
     async findOne(options: FindOneOptions<T>) {
-        SendEvent(`Starting search for one ${this.name} in database!`, {}, 'info');
+        SendEvent(`${this.constructor.name} - Starting search for one ${this.name} in database!`, {}, 'info');
 
         const models = this.repository.findOne(options);
         models.then(result => {
             if(result)
-                SendEvent(`Search for one ${this.name} completed successfully!`, options);
+                SendEvent(`${this.constructor.name} - Search for one ${this.name} completed successfully!`, options);
             else
-                SendEvent(`Has no ${this.name} in database!`, options, 'warn');
+                SendEvent(`${this.constructor.name} - Has no ${this.name} in database!`, options, 'warn');
         }).catch((error) => {
-            SendEvent(`Search for ${this.name} has a error!`, error, 'error');
+            SendEvent(`${this.constructor.name} - Search for ${this.name} has a error!`, error, 'error');
         });
 
         return models;
     }
 
     async findBy(params : FindOptionsWhere<T> | FindOptionsWhere<T>[]){
-        SendEvent(`Starting search by params for ${this.name} in database!`, params, 'info');
+        SendEvent(`${this.constructor.name} - Starting search by params for ${this.name} in database!`, params, 'info');
 
         const models = this.repository.findBy(params);
         models.then(result => {
             if(result)
-                SendEvent(`Search by params for ${this.name} completed successfully!`, params);
+                SendEvent(`${this.constructor.name} - Search by params for ${this.name} completed successfully!`, params);
             else
-                SendEvent(`Has no ${this.name} with this params in database!`, params, 'warn');
+                SendEvent(`${this.constructor.name} - Has no ${this.name} with this params in database!`, params, 'warn');
         }).catch((error) => {
-            SendEvent(`Search for ${this.name} has a error!`, error, 'error');
+            SendEvent(`${this.constructor.name} - Search for ${this.name} has a error!`, error, 'error');
         });
 
         return models;
     }
 
     async findOneBy(params : {}){
-        SendEvent(`Starting search by params for one ${this.name} in database!`, params, 'info');
+        SendEvent(`${this.constructor.name} - Starting search by params for one ${this.name} in database!`, params, 'info');
 
         const models = this.repository.findOneBy(params);
         models.then(result => {
             if(result)
-                SendEvent(`Search by params for one ${this.name} completed successfully!`, params);
+                SendEvent(`${this.constructor.name} - Search by params for one ${this.name} completed successfully!`, params);
             else
-                SendEvent(`Has no ${this.name} with this params in database!`, params, 'warn');
+                SendEvent(`${this.constructor.name} - Has no ${this.name} with this params in database!`, params, 'warn');
         }).catch((error) => {
-            SendEvent(`Search for one ${this.name} has a error!`, error, 'error');
+            SendEvent(`${this.constructor.name} - Search for one ${this.name} has a error!`, error, 'error');
         });
 
         return models;
@@ -82,22 +82,22 @@ export abstract class BaseService<T extends BaseEntity> implements Service<T> {
     }
 
     async save(model : T){
-        SendEvent(`Starting saving a new item of ${this.name} in database!`, model);
+        SendEvent(`${this.constructor.name} - Starting saving a new item of ${this.name} in database!`, model);
 
         let promiseResult = this.repository.save(model).then(this.checkIfModelHasId).catch((error) => {
-            SendEvent(`Has no ${this.name}`, error, 'error');
+            SendEvent(`${this.constructor.name} - Has no ${this.name}`, error, 'error');
         });
 
         return promiseResult;
     }
 
     async saveAll(models : T[]){
-        SendEvent(`Starting saving a new item of ${this.name} in database!`, models);
+        SendEvent(`${this.constructor.name} - Starting saving a new item of ${this.name} in database!`, models);
 
         let promiseResult = this.repository.save(models)
         .then((result) => result.forEach((eachResult) => this.checkIfModelHasId(eachResult)))
         .catch((error) => {
-            SendEvent(`Has no ${this.name}`, error, 'error');
+            SendEvent(`${this.constructor.name} - Has no ${this.name}`, error, 'error');
         });
 
         return promiseResult;
@@ -121,9 +121,9 @@ export abstract class BaseService<T extends BaseEntity> implements Service<T> {
 
     private checkIfModelHasId(model : T){
         if(model.hasId())
-            SendEvent(`One item of ${this.name} was saved in database with successfully!`, model);
+            SendEvent(`${this.constructor.name} - One item of ${this.name} was saved in database with successfully!`, model);
         else
-            SendEvent(`Item has no ID. Check table ${this.name} is the model exist!`, model, 'warn');
+            SendEvent(`${this.constructor.name} - Item has no ID. Check table ${this.name} is the model exist!`, model, 'warn');
     }
 
 }
